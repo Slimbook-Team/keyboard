1c0efed3a63107b5800cbb54c53de5e14eb4f4ad
SLIMBOOK TEAM <sat@slimbook.es>
2020-10-04 14:46:14 +0200

    Create install_rgb.sh

---
+++ install_rgb.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+git clone https://github.com/slimbook/keyboard.git
+
+cd keyboard/backlight/essential/rgb-module/module/
+
+sudo apt install gcc make build-essential
+
+make && sudo make install
+
+sudo insmod clevo-xsm-wmi.ko
+
+sudo install -m644 clevo-xsm-wmi.ko /lib/modules/$(uname -r)/extra
+
+sudo depmod
+
+sudo tee /etc/modules-load.d/clevo-xsm-wmi.conf <<< clevo-xsm-wmi
+
+sudo update-initramfs -uk all
+
+sudo tee /etc/modprobe.d/clevo-xsm-wmi.conf <<< 'options clevo-xsm-wmi kb_color=white,white,white, kb_brightness=10'
